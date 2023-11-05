09a7e8eb814509aec811506aea0305c36b2666d0
lilyana-kyuchukova <121547737+lilyana-kyuchukova@users.noreply.github.com>
2023-11-05 15:47:17 +0200

    updated file

--- encapsulation_lab/profile.py
+++ encapsulation_lab/profile.py
@@ -15,22 +15,25 @@ class Profile:
         return self.__password
 
     @username.setter
-    def username(self, username: str):
-        if 5 <= len(username) <= 15:
-            self.__username = username
-        else:
+    def username(self, value: str):
+        if not 5 <= len(value) <= 15:
             raise ValueError("The username must be between 5 and 15 characters.")
+        self.__username = value
 
     @password.setter
-    def password(self, password: str):
-        if len(password) >= 8 and re.search(r"\d+", password) and re.search(r"[A-Z]+", password):
-            self.__password = password
+    def password(self, value: str):
+        # value.lower == value True
+        # [l for l in value if l.isdigit()] True
+        if len(value) >= 8 and re.search(r"\d+", value) and re.search(r"[A-Z]+", value):
+            self.__password = value
         else:
             raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
 
     def __str__(self):
         s = ["*" for st in range(len(self.password))]
         return f"You have a profile with username: \"{self.username}\" and password: {''.join(s)}"
+
+        # return f"You have a profile with username: \"{self.username}\" and password: {'*' * len(password)}"
 
 
 # profile_with_invalid_password = Profile('My_username', 'My-password')
